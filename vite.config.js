391f876d6d7d0376e1841a83eacfec8f786161e9
iliutaadrian <iliuta.podoleanu@golfgenius.com>
2025-11-20 13:36:53 +0200

    fix: base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/options_trading_simulator/',
   server: {
     host: true,
     port: 5173,
