180afa1fac8f4bdfb35b00a3654799d63e49c289
iliutaadrian <iliuta.podoleanu@golfgenius.com>
2025-11-22 14:58:07 +0200

    fix: chart height size

--- src/components/PriceChart.jsx
+++ src/components/PriceChart.jsx
@@ -259,7 +259,7 @@ const PriceChart = ({ data, currentIndex, vixData, indicator }) => {
             )}
           </div>
         </div>
-        <ResponsiveContainer width="100%" height={450}>
+        <ResponsiveContainer width="100%" height={350}>
           <ComposedChart data={dataWithVolumeSMA} margin={{ top: 10, right: 0, left: 0, bottom: 0 }}>
             <defs>
               <linearGradient id="priceGradient" x1="0" y1="0" x2="0" y2="1">
@@ -522,7 +522,7 @@ const PriceChart = ({ data, currentIndex, vixData, indicator }) => {
       </div>
 
       <div className="chart-section tradingview-style">
-        <ResponsiveContainer width="100%" height={120}>
+        <ResponsiveContainer width="100%" height={100}>
           <ComposedChart data={dataWithVolumeSMA} margin={{ top: 5, right: 0, left: 0, bottom: 0 }}>
             <CartesianGrid strokeDasharray="3 3" stroke="#1e2940" strokeWidth={1} />
             <XAxis
@@ -561,7 +561,7 @@ const PriceChart = ({ data, currentIndex, vixData, indicator }) => {
       </div>
 
       <div className="chart-section tradingview-style">
-        <ResponsiveContainer width="100%" height={120}>
+        <ResponsiveContainer width="100%" height={100}>
           <ComposedChart data={dataWithVolumeSMA} margin={{ top: 5, right: 0, left: 0, bottom: 0 }}>
             <CartesianGrid strokeDasharray="3 3" stroke="#1e2940" strokeWidth={1} />
             <XAxis
